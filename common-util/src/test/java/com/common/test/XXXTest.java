579af888ff2230a98a66ec35463c61686a3414ca
luowei <luowei505050@126.com>
2012-12-29 19:49:19 +0800

    添加了commonUtil。。。。

---
+++ common-util/src/test/java/com/common/test/XXXTest.java
@@ -0,0 +1,11 @@
+package com.common.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: luowei
+ * Date: 12-12-29
+ * Time: 下午7:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class XXXTest {
+}
